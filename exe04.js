bec01e19931a92c7d3622ec4b6dfa7b222d7a1d6
cristiano <cristianocoffy@gmail.com>
2025-06-03 23:52:27 -0300

    Concluindo exercicio 04

---
+++ exe04.js
@@ -0,0 +1,16 @@
+/**
+ * 4. Fatorial Recursivo Implemente function fatorial(n) 
+ * de forma recursiva; trate n < 0 lançando um Error, e n === 0 retornando 1.
+ */
+
+function fatorial(n){
+    if(n < 0) {
+   throw RangeError("N menos que zero")
+  }
+    if(n===0) return 1
+
+    return n * fatorial(n - 1)
+}
+
+
+console.log(fatorial(-2))
